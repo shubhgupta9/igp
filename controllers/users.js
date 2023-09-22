09047b95a754f63e3c6c6228bef3208985a6b621
Shubh Gupta <shubh.g0279@gmail.com>
2023-09-22 15:00:17 +0530

    photo

--- controllers/users.js
+++ controllers/users.js
@@ -2,18 +2,34 @@ const fs = require("fs");
 const data1 = JSON.parse(fs.readFileSync("data1.json", "utf-8"));
 const users = data1.users;
 const multer = require("multer");
-const upload = multer();
 
 function saveDataToFile() {
   fs.writeFileSync("data1.json", JSON.stringify(data1, null, 2), "utf-8");
 }
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 8 * 1024 * 1024 }, // 8MB file size limit
+});
+
 exports.create = (req, res) => {
   const { name, email, designation } = req.body;
 
-  // if (!name || !email || !designation) {
-  //   return res.status(400).send("Please provide all required fields.");
-  // }
+  if (!name || !email || !designation) {
+    alert("pls provide all fields");
+    return res.status(400).send("Please provide all required fields.");
+  }
+
   const newUser = {
     id: users.length + 1,
     name,
@@ -44,13 +60,27 @@ exports.readAll = (req, res) => {
 };
 
 exports.update = [
-  upload.none(),
+  upload.single("photo"),
   (req, res) => {
     const id = +req.params.id;
     const userIndex = users.findIndex((p) => p.id === id);
+
+    const { name, email, designation } = req.body;
+
+    if (!name || !email || !designation) {
+      return res.status(400).send("Please provide all required fields.");
+    }
+
     const updatedUser = users[userIndex];
+
+    if (req.file) {
+      // Handle file upload if a new photo is provided
+      updatedUser.photo = req.file.filename;
+    }
+
     users.splice(userIndex, 1, { ...updatedUser, ...req.body });
     saveDataToFile();
+    return res.status(200).send("OKAY");
     // res.redirect("/");
   },
 ];
@@ -64,6 +94,11 @@ exports.renderEditUserForm = (req, res) => {
 exports.delete = (req, res) => {
   const id = +req.params.id;
   const userIndex = users.findIndex((p) => p.id === id);
+
+  if (userIndex === -1) {
+    return res.status(404).send("User not found.");
+  }
+
   users.splice(userIndex, 1);
   saveDataToFile();
   res.redirect("/");
